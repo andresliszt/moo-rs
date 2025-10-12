0adbc209ec0fa3a606c9ff3b44edb615bd15d06a
andresliszt <andres.sndbrca@gmail.com>
2025-10-12 12:07:23 -0300

    Backup

--- pymoors/src/algorithms/moo/mod.rs
+++ pymoors/src/algorithms/moo/mod.rs
@@ -4,6 +4,7 @@ mod nsga3;
 mod revea;
 mod rnsga2;
 mod spea2;
+mod ibea;
 
 pub use agemoea::PyAgeMoea;
 pub use nsga2::PyNsga2;
@@ -11,3 +12,4 @@ pub use nsga3::PyNsga3;
 pub use revea::PyRevea;
 pub use rnsga2::PyRnsga2;
 pub use spea2::PySpea2;
+pub use ibea::PyIbea;
